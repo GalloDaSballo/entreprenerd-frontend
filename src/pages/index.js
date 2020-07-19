04fc2de8c384fe85a03ad1aee5231892441b83c6
Entreprenerd <alex@entreprenerd.xyz>
2020-07-19 16:58:19 +0200

    Changed Site Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>What's up I'm Alex the Entreprenerd, this is my site, basically a bunch of free stuff for you</h1>
     <h3><ExternalLink to="https://www.udemy.com/course/the-complete-strapi-course/" title="The Complete Strapi Course" /></h3>
+    <h3><ExternalLink to="https://www.youtube.com/c/AlexTheEntreprenerd" title="Youtube" /></h3>
     <h3><ExternalLink to="https://github.com/GalloDaSballo" title="Github" /></h3>
     <h3><ExternalLink to="https://www.codementor.io/@alessandrovalerani" title="Codementor" /></h3>
   </Layout>
